01b76414d532229f2c377ba5349e3056afdbb19f
gleidsonalencar <52585397+gleidsonalencar@users.noreply.github.com>
2021-06-20 18:04:45 -0300

    Lista de veículos cadastrados

---
+++ Controller/concessionaria/ListarVeiculos.java
@@ -0,0 +1,55 @@
+package concessionaria;
+
+import jdk.internal.jshell.tool.resources.version;
+
+public class ListarVeiculos {
+
+    public ListarVeiculos() {
+
+    }
+
+    public void listarPicape(Picape picape) {
+        picape.getNome();
+		picape.getCor();
+		picape.getPreco();
+		
+		System.out.println(
+				"\nNome: "+ picape.getNome() +
+				"\nCor: "+ picape.getCor() +
+				"\nPreco: "+ picape.getPreco());
+    }
+
+    public void listarSUV(SUV suv) {
+        suv.getNome();
+		suv.getCor();
+		suv.getPreco();
+		
+		System.out.println(
+				"\nNome: "+ suv.getNome() +
+				"\nCor: "+ suv.getCor() +
+				"\nPreco: "+ suv.getPreco());
+    }
+
+    public void listarSedan(Sedan sedan) {
+        sedan.getNome();
+		sedan.getCor();
+		sedan.getPreco();
+		
+		System.out.println(
+				"\nNome: "+ sedan.getNome() +
+				"\nCor: "+ sedan.getCor() +
+				"\nPreco: "+ sedan.getPreco());
+    }
+
+    public void listarHatch(Hatch hatch) {
+        hatch.getNome();
+		hatch.getCor();
+		hatch.getPreco();
+		
+		System.out.println(
+				"\nNome: "+ hatch.getNome() +
+				"\nCor: "+ hatch.getCor() +
+				"\nPreco: "+ hatch.getPreco());
+    }
+    
+}
