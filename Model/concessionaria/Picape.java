06d803e22bdc8021c35a31e4b83788e58680f987
gleidsonalencar <52585397+gleidsonalencar@users.noreply.github.com>
2021-06-20 18:02:08 -0300

    Tipo de veículo - Picape

---
+++ Model/concessionaria/Picape.java
@@ -0,0 +1,20 @@
+package concessionaria.Model.concessionaria;
+
+public class Picape extends Veiculo {
+
+    Double preco;
+
+    public Picape(String nome, String cor, Double preco) {
+        super(nome, cor);
+        this.setPreco(preco);
+    }
+
+    public Double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(Double preco) {
+        this.preco = preco;
+    }
+
+}
