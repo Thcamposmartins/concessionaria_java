1a88bd554ea27e4861bcec7ac125d011ade3f4cd
gleidsonalencar <52585397+gleidsonalencar@users.noreply.github.com>
2021-06-20 18:05:10 -0300

    Lista de vendas realizadas

---
+++ Controller/concessionaria/ListarVenda.java
@@ -0,0 +1,22 @@
+package concessionaria;
+
+public class ListarVenda {
+    
+    public ListarVenda() {
+
+    }
+
+    public void listarVendas(Venda venda) {
+        venda.getQuantidadeDeVendas();
+		venda.getNomeVeiculoVendido();
+		venda.getCorVeiculoVendido();
+        venda.getPrecoVeiculoVendido();
+		
+		System.out.println(
+				"\nQuantidade de vendas: " + venda.getQuantidadeDeVendas() +
+				"\nNome do veiculo vendido: " + venda.getNomeVeiculoVendido() +
+				"\nCor do veiculo vendido: " + venda.getCorVeiculoVendido(); +
+                "\nPreco do veiculo vendido:" + venda.getPrecoVeiculoVendido());
+    }
+
+}
